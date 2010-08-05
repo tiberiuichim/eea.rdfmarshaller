f5c4a15325c3cd68fab07d9a79d66eb844b15b37
Alexandru Ghica <alec.ghica@eaudeweb.ro>
2010-08-05 09:09:03 +0000

    fix typo

--- eea/rdfmarshaller/marshaller.py
+++ eea/rdfmarshaller/marshaller.py
@@ -79,7 +79,7 @@ class ATCT2Surf(object):
     def blacklist_map(self):
         ptool = getToolByName(self.context,'portal_properties')
         props = getattr(ptool, 'rdfmarshaller_properties', None)
-        #TODO: added 'file' to back list as fails with unicode error
+        #TODO: added 'file' to blacklist as fails with unicode error
         blacklist = ['constrainTypesMode','locallyAllowedTypes', 'immediatelyAddableTypes','language', 'allowDiscussion', 'file'] # fields not to export
         if props:
             blacklist = list(props.getProperty('%s_blacklist' % self.portalType.lower(), props.getProperty('blacklist')))
