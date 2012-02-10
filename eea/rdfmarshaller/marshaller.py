7dc238590ef98e8bd7f88e5461db6d88c1902df7
tiberich <tiberiuichim@users.noreply.github.com>
2012-02-10 09:41:18 +0000

    Refs #8452; reverted previous commit

--- eea/rdfmarshaller/marshaller.py
+++ eea/rdfmarshaller/marshaller.py
@@ -193,8 +193,6 @@ class ATCT2Surf(object):
         resource = self.surfResource
         language = context.Language()
 
-        setattr(resource, '%s_%s' % ('dcterms', 'language'), language)
-
         for field in context.Schema().fields():
             fieldName = field.getName()
             if fieldName in self.blacklist_map:
@@ -223,16 +221,13 @@ class ATCT2Surf(object):
                         value = (value.HTML4(), None,
                                 'http://www.w3.org/2001/XMLSchema#dateTime')
                     elif isinstance(value, str):
-                        #value = (value, language)
-                        value = (value, None)   #removed language
+                        value = (value, language)
                     elif isinstance(value, unicode):
                         pass
                     else:
                         try:
-                            #value = (unicode(value, 'utf-8', 'replace'),
-                                    #language)
                             value = (unicode(value, 'utf-8', 'replace'),
-                                    None)   #removed language
+                                    language)
                         except TypeError:
                             value = str(value)
 
