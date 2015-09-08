525d369d0dcb111944256856d93403b7c0a69f52
Dragos Chirila <DragosChirila@users.noreply.github.com>
2015-09-08 06:45:43 +0000

    Fixed Pylint warnings
    Bug fix: fixed UnicodeDecodeError: 'utf8' codec can't decode byte for @@rdf
    [chiridra refs #28293]

--- eea/rdfmarshaller/archetypes/modifiers.py
+++ eea/rdfmarshaller/archetypes/modifiers.py
@@ -10,7 +10,9 @@ from zope.interface import implements, providedBy
 import rdflib
 import re
 
-ILLEGAL_XML_CHARS_PATTERN = re.compile(u'[\x00-\x08\x0b\x0c\x0e-\x1F\uD800-\uDFFF\uFFFE\uFFFF]')
+ILLEGAL_XML_CHARS_PATTERN = re.compile(
+    u'[\x00-\x08\x0b\x0c\x0e-\x1F\uD800-\uDFFF\uFFFE\uFFFF]'
+)
 
 class IsPartOfModifier(object):
     """Adds dcterms_isPartOf information to rdf resources
@@ -102,5 +104,6 @@ class SearchableTextInModifier(object):
         """Change the rdf resource
         """
 
-        resource.dcterms_abstract = ILLEGAL_XML_CHARS_PATTERN.sub('', self.context.SearchableText())
+        resource.dcterms_abstract = ILLEGAL_XML_CHARS_PATTERN.sub('',
+            self.context.SearchableText())
 
