0b9cd8c2adb98e0e8557d76c4fb6ad175f536df9
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2016-10-14 13:46:47 +0200

    Refs #77766; fixed non ascii characters for rdf export

--- eea/rdfmarshaller/browser/export.py
+++ eea/rdfmarshaller/browser/export.py
@@ -1,4 +1,5 @@
 """ Export module """
+from lxml import etree
 from Products.Marshall.registry import getComponent
 
 
@@ -14,9 +15,12 @@ class RDFExport(object):
         endLevel = int(self.request.get('endLevel', 1))
         _content_type, _length, data = marshaller.marshall(self.context,
                                                            endLevel=endLevel)
+        #fix for #77766; fix non ascii characters for rdf export
+        sanitized_data = etree.tostring(etree.fromstring(data))
+        #end of fix
         self.request.response.setHeader('Content-Type',
                                         'application/rdf+xml; charset=utf-8')
-        return data
+        return sanitized_data
 
 class RDFSExport(object):
     """ RDF Surf export """
