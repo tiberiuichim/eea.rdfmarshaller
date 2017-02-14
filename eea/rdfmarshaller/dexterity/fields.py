61327ac11432dd08b41b9630d30058d4b2ea62a0
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2017-02-14 14:26:27 +0200

    Refs #79312 CCA; added rdf implementation for z3c.relationfield

--- eea/rdfmarshaller/dexterity/fields.py
+++ eea/rdfmarshaller/dexterity/fields.py
@@ -1,11 +1,13 @@
 from Acquisition import aq_base
 from Products.CMFPlone import log
-from eea.rdfmarshaller.value import Value2Surf
-from eea.rdfmarshaller.interfaces import IFieldDefinition2Surf
 from eea.rdfmarshaller.dexterity.interfaces import IDXField2Surf
+from eea.rdfmarshaller.interfaces import IFieldDefinition2Surf
 from eea.rdfmarshaller.interfaces import ISurfSession
 from eea.rdfmarshaller.marshaller import GenericObject2Surf
+from eea.rdfmarshaller.value import Value2Surf
 from plone.app.textfield.value import RichTextValue
+from z3c.relationfield.interfaces import IRelationList
+from z3c.relationfield.interfaces import IRelationValue
 from zope.component import adapts
 from zope.interface import implements, Interface
 from zope.schema.interfaces import IField
@@ -102,3 +104,33 @@ class DexterityField2RdfSchema(GenericObject2Surf):
         setattr(resource, 'rdf_id', self.rdfId)
         setattr(resource, 'rdf_domain', fti_title)
         return resource
+
+
+class DXRelationList2Surf(DXField2Surf):
+    """IATField2Surf implementation for Reference fields"""
+
+    adapts(IRelationList, Interface, ISurfSession)
+
+    def value(self):
+        """ Value """
+        value = super(DXRelationList2Surf, self).value()
+
+        # some reference fields are single value only
+        if not isinstance(value, (list, tuple)):
+            value = [value]
+
+        value = [v for v in value if v]     # the field might have been empty
+
+        return [rdflib.URIRef(ref.to_object.absolute_url()) for ref in value]
+
+
+class RelationValue2Surf(Value2Surf):
+    """IValue2Surf implementation for DateTime """
+
+    adapts(IRelationValue)
+
+    def __call__(self, *args, **kwds):
+
+        value = self.value
+        obj = value.to_object
+        return rdflib.URIRef(obj.absolute_url())
